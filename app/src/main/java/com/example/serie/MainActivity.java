3ca522394b6ed740c531a2d41abb06d4a3ec7e58
JorgeHM28 <liz388530@gmail.com>
2026-04-28 23:16:52 -0400

    feat: implementación de lógica matemática en Java

--- app/src/main/java/com/example/serie/MainActivity.java
+++ app/src/main/java/com/example/serie/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.serie;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,81 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText etN;
+    Button btnGenerar;
+    TextView tvResultado;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        etN = findViewById(R.id.etN);
+        btnGenerar = findViewById(R.id.btnGenerar);
+        tvResultado = findViewById(R.id.tvResultado);
+
+        btnGenerar.setOnClickListener(v -> {
+
+            String texto = etN.getText().toString();
+
+            if (texto.isEmpty()) {
+                tvResultado.setText("Error: campo vacío");
+                return;
+            }
+
+            int n;
+
+            try {
+                n = Integer.parseInt(texto);
+            } catch (Exception e) {
+                tvResultado.setText("Error: valor no numérico");
+                return;
+            }
+
+            if (n <= 0) {
+                tvResultado.setText("Error: n debe ser mayor a 0");
+                return;
+            }
+
+            String resultado = generarSerie(n);
+            tvResultado.setText(resultado);
         });
+    }
+
+    public String generarSerie(int n) {
+        StringBuilder resultado = new StringBuilder();
+        double valor = 1;
+        double incremento = 1.5;
+        double suma = 0;
+
+        for (int i = 1; i <= n; i++) {
+
+            double termino;
+
+            if (i == 1) {
+                termino = valor;
+            } else {
+                valor = valor + incremento;
+                incremento += 1;
+                termino = valor;
+            }
+
+            // Alternar signo
+            if (i % 2 == 0) {
+                termino *= -1;
+            }
+
+            suma += termino;
+
+            resultado.append(termino);
+
+            if (i < n) {
+                resultado.append(", ");
+            }
+        }
+
+        resultado.append("\nSuma: ").append(suma);
+
+        return resultado.toString();
     }
 }
